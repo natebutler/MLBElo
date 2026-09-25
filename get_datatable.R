32d75b409a41dbf21c61680bfa47adf4052e9501
natebutler <nathan.butler@usu.edu>
2026-09-24 20:39:28 -0500

    updating cron job

--- get_datatable.R
+++ get_datatable.R
@@ -226,7 +226,7 @@ update_elo_ratings <- function(regress = 0.75, start_date = "2026-03-24", end_da
 ################################################################################
 ###### Get Data
 
-elo_results <- update_elo_ratings(regress = 0.2, k = 7, start_date = "2026-03-24")
+elo_results <- update_elo_ratings(regress = 0.14, k = 7, start_date = "2026-03-24")
 
 team_logos <- load_mlb_teams() %>% 
   select(team_name, team_abbr, team_color, team_logo_espn)
